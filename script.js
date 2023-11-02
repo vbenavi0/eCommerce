07e8465757da80e3f7f2bd284b00ff26a13d333b
vbenavi0 <victorvill.0988@gmail.com>
2023-11-02 04:12:39 -0400

    added cart functionality

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let index = 1;
-let heroId;
-let prevHero;
+var index = 1;
+var heroId;
+var prevHero;
 
-let uName;
-let uEmail;
-let uQuestion;
-let uComment;
-let warning;
+var uName;
+var uEmail;
+var uQuestion;
+var uComment;
+var warning;
 
-let cart = [];
-let subTotal = 0;
+var subTotal = 0;
+var counter = 0;
 
-function changeHero(){
+function changeHero(){//change hero img
     document.getElementById('hiddenImg').style.opacity = 0;
     heroId = 'hero' + index;
     document.getElementById(heroId).style.opacity = 0;
@@ -26,11 +26,11 @@ function changeHero(){
 
 }
 
-if (window.location.pathname.includes('/index.html')){
-    setInterval(changeHero, 3000);
+if (window.location.pathname.includes('/index.html')){ //if index.html is being viewed
+    setInterval(changeHero, 3000); //perform changeHero every 3 secs
 }
 
-function formSubmit(){
+function formSubmit(){ //form validation
     warning = "";
     document.getElementById('warning').innerHTML = ""
     uName = document.getElementById('uName').value;
@@ -58,21 +58,27 @@ function formSubmit(){
     }
 }
 
-function addToCart(item){
-    cart.push(item);
-    alert('Added to shopping cart.')
-}
-
-function showCart(){
-    console.log('show '+ cart);
+function addToCart(item, price){ //Adds items to cart
+    console.log(item+' has been added to cart');
+    alert('Added to shopping cart.');
     let docCart = document.getElementById('cart');
-    cart.forEach(item=>{
-        newItem = document.createElement("div");
-        newItem.innerHTML = ''
-
-    })
+    let newItem = document.createElement('p');
+    newItem.textContent = item + " : $" +price;
+    console.log(newItem);
+    docCart.appendChild(newItem);
+    subTotal += price;
+    document.getElementById('subTotal').textContent = 'Subtotal: $'+ subTotal;
 }
 
-if (window.location.pathname.includes('/cart.html')){
-    showCart();
+function showCart(){ //shows or hides cart
+    console.log('show')
+    var cartEl = document.getElementById('cart');
+    if(counter%2 === 0){
+        document.getElementById('cart').style.zIndex ='1';
+        counter++
+    }
+    else if(counter%2 === 1){
+        document.getElementById('cart').style.zIndex =-'1';
+        counter++
+    }
 }
